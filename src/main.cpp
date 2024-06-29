d37ae081e6524939f801bac53741f862fcc60105
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-06-30 00:37:20 +0300

    recoding in class

--- src/main.cpp
+++ src/main.cpp
@@ -1,146 +1,54 @@
-#include <algorithm>
 #include <cstring>
-#include <fstream>
 #include <iostream>
 #include <string>
-#include <tuple>
-#include <vector>
 
-using ip_addr = std::tuple<int, int, int, int>;
-using ip_addrs = std::vector<ip_addr>;
-
-ip_addrs ip_addrs_pool;
-
-auto extract_element(std::string sSourceString) {
-
-  std::vector<int> iReturnValue;
-  std::string sReturnString{};
-
-  for (char symbol : sSourceString) {
-    switch (symbol) {
-    case '\t':
-      iReturnValue.push_back(std::stoi(sReturnString));
-      break;
-    case '.':
-      iReturnValue.push_back(std::stoi(sReturnString));
-      sReturnString.erase();
-      break;
-    default:
-      sReturnString.push_back(symbol);
-      break;
-    }
-  }
-  return std::make_tuple(iReturnValue[0], iReturnValue[1], iReturnValue[2],
-                         iReturnValue[3]);
-}
-
-bool IPAddrGE(ip_addr ConditionValueL, ip_addr ConditionValueR) {
-
-  return std::get<0>(ConditionValueL) > std::get<0>(ConditionValueR);
-}
-
-bool IPAddrLE(ip_addr ConditionValueL, ip_addr ConditionValueR) {
-
-  if (std::get<0>(ConditionValueL) > std::get<0>(ConditionValueR)) {
-    return true;
-  }
-
-  if (std::get<0>(ConditionValueL) == std::get<0>(ConditionValueR)) {
-    if (std::get<1>(ConditionValueL) > std::get<1>(ConditionValueR)) {
-      return true;
-    }
-  }
-
-  if (std::get<1>(ConditionValueL) == std::get<1>(ConditionValueR)) {
-    if (std::get<2>(ConditionValueL) > std::get<2>(ConditionValueR)) {
-      return true;
-    }
-  }
-
-  if (std::get<2>(ConditionValueL) == std::get<2>(ConditionValueR)) {
-    if (std::get<3>(ConditionValueL) > std::get<3>(ConditionValueR)) {
-      return true;
-    }
-  }
-
-  if (std::get<3>(ConditionValueL) == std::get<3>(ConditionValueR)) {
-    return true;
-  }
-
-  return false;
-}
-
-void print_element(ip_addrs IPAddr) {
-
-  for (auto pos : IPAddr) {
-    std::cout << std::get<0>(pos) << "." << std::get<1>(pos) << "."
-              << std::get<2>(pos) << "." << std::get<3>(pos) << std::endl;
-  }
-}
-
-void print_element(ip_addrs IPAddr, int iOct1, int iOct2) {
-
-  for (auto pos : IPAddr) {
-    if (std::get<0>(pos) == iOct1 && std::get<1>(pos) == iOct2) {
-      std::cout << std::get<0>(pos) << "." << std::get<1>(pos) << "."
-                << std::get<2>(pos) << "." << std::get<3>(pos) << std::endl;
-    }
-  }
-}
-
-void print_element(ip_addrs IPAddr, int iAnyOct) {
-
-  for (auto pos : IPAddr) {
-    if (std::get<0>(pos) == iAnyOct || std::get<1>(pos) == iAnyOct ||
-        std::get<2>(pos) == iAnyOct || std::get<3>(pos) == iAnyOct) {
-      std::cout << std::get<0>(pos) << "." << std::get<1>(pos) << "."
-                << std::get<2>(pos) << "." << std::get<3>(pos) << std::endl;
-    }
-  }
-}
+#include <sort_ip.hpp>
 
 int main() {
+
+  Object obj;
 
   try {
 
     for (std::string line; std::getline(std::cin, line);) {
-      ip_addrs_pool.push_back(extract_element(line));
+      obj.push_back(line);
     }
 
     std::cout << "0. Полный список адресов без сортировки. Одна строка - "
                  "один адрес."
               << std::endl;
 
-    print_element(ip_addrs_pool);
+    obj.print_element();
 
     std::cout << "1. Полный список адресов после сортировки. Одна строка - "
                  "один адрес."
               << std::endl;
 
-    std::sort(ip_addrs_pool.begin(), ip_addrs_pool.end(), IPAddrLE);
+    obj.sort();
 
-    print_element(ip_addrs_pool);
+    obj.print_element();
 
     std::cout << "2. Сразу следом список адресов, первый байт которых равен "
                  "1. Порядок сортировки не меняется. Одна строка - один "
                  "адрес. Списки ничем не разделяются."
               << std::endl;
 
-    print_element(ip_addrs_pool, 1);
+    obj.print_element(1);
 
     std::cout << "3. Сразу продолжается список адресов, первый байт которых "
                  "равен 46, а второй 70. Порядок сортировки не меняется. "
                  "Одна строка - один адрес. Списки ничем не разделяются."
               << std::endl;
 
-    print_element(ip_addrs_pool, 46, 70);
+    obj.print_element(46, 70);
 
     std::cout << "4. Сразу продолжается список адресов, любой байт которых "
                  "равен 46. Порядок сортировки не меняется. Одна строка - "
                  "один адрес. Списки ничем не разделяются."
               << std::endl;
 
-    print_element(ip_addrs_pool, 46);
+    obj.print_element(46, 46, 46, 46);
+
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
