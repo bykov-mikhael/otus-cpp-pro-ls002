acb635fd14fdcf0560aab3391c74d4ffcaf509cb
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-06-30 16:41:25 +0300

    tests

--- tests/gtests_version.cpp
+++ tests/gtests_version.cpp
@@ -8,28 +8,4 @@
 
 // TEST(gtest, test_version) { ASSERT_LT(version(), 100); }
 
-TEST(gtest, read_file) {
-
-  // std::ifstream in("ip_filter.tsv");
-
-  // Object obj;
-
-  // try {
-
-  //   if (in.is_open()) {
-  //     std::string line;
-
-  //     while (std::getline(in, line, '\n')) {
-  //       obj.push_back(line);
-  //     }
-
-  //     auto t = std::make_tuple(0, 0, 0, 0);
-
-  //     ASSERT_EQ(obj[1], t);
-  //   }
-  // } catch (const std::exception &e) {
-  //   std::cerr << e.what() << std::endl;
-  //   in.close();
-  // }
-  // in.close();
-}
+TEST(gtest, read_file) {}
