acbc3a336ba0c7cb00f3b6ce45557510146b0983
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-06-30 15:34:01 +0300

    remove tests

--- tests/gtests_version.cpp
+++ tests/gtests_version.cpp
@@ -10,26 +10,26 @@
 
 TEST(gtest, read_file) {
 
-  std::ifstream in("ip_filter.tsv");
+  // std::ifstream in("ip_filter.tsv");
 
-  Object obj;
+  // Object obj;
 
-  try {
+  // try {
 
-    if (in.is_open()) {
-      std::string line;
+  //   if (in.is_open()) {
+  //     std::string line;
 
-      while (std::getline(in, line, '\n')) {
-        obj.push_back(line);
-      }
+  //     while (std::getline(in, line, '\n')) {
+  //       obj.push_back(line);
+  //     }
 
-      auto t = std::make_tuple(0, 0, 0, 0);
+  //     auto t = std::make_tuple(0, 0, 0, 0);
 
-      ASSERT_EQ(obj[1], t);
-    }
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    in.close();
-  }
-  in.close();
+  //     ASSERT_EQ(obj[1], t);
+  //   }
+  // } catch (const std::exception &e) {
+  //   std::cerr << e.what() << std::endl;
+  //   in.close();
+  // }
+  // in.close();
 }
