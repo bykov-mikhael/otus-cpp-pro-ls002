d1d5b07379e39dceb78e84e3ce2775558ecd4ab5
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-08-21 20:37:37 +0300

    first commit

---
+++ tests/gtests_version.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include <fstream>
+
+#include <lib.h>
+#include <sort_ip.hpp>
+#include <tuple>
+
+// TEST(gtest, test_version) { ASSERT_LT(version(), 100); }
+
+TEST(gtest, read_file) {}
